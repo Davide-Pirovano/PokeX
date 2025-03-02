5496ff9d40137a2b46092a360b58287e742b7aff
Davide Pirovano <60658968+Davide-Pirovano@users.noreply.github.com>
2025-03-02 11:15:00 +0100

    fix_loading

--- lib/pages/pokemon_details_page.dart
+++ lib/pages/pokemon_details_page.dart
@@ -48,97 +48,88 @@ class _PokemonDetailsPageState extends State<PokemonDetailsPage> {
 
         return Scaffold(
           backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          body:
-              _isFetchingDetails &&
-                      (updatedPokemon.height == null ||
-                          updatedPokemon.weight == null)
-                  ? const Center(child: CircularProgressIndicator())
-                  : Column(
+          body: Column(
+            children: [
+              /// **AppBar fissa**
+              Container(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top,
+                ),
+                color: backgroundColor,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 16,
+                  ),
+                  child: Column(
                     children: [
-                      /// **AppBar fissa**
-                      Container(
-                        padding: EdgeInsets.only(
-                          top: MediaQuery.of(context).padding.top,
-                        ),
-                        color: backgroundColor,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 8,
-                            vertical: 16,
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.arrow_back,
-                                      size: 32,
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                    ),
-                                    onPressed: () => Navigator.pop(context),
-                                    padding: EdgeInsets.zero,
-                                    constraints: const BoxConstraints(),
-                                    splashRadius: 24,
-                                  ),
-                                  FavSelector(pokemon: updatedPokemon),
-                                ],
-                              ),
-                              const SizedBox(height: 16),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0,
-                                ),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      updatedPokemon.name.capitalize(),
-                                      style: const TextStyle(fontSize: 30),
-                                    ),
-                                    const SizedBox(width: 8),
-                                    Text(
-                                      updatedPokemon.id! < 100
-                                          ? "#0${updatedPokemon.id}"
-                                          : "#${updatedPokemon.id}",
-                                      style: TextStyle(
-                                        fontSize: 30,
-                                        color: ColorUtil().lightGrey,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-
-                      /// **Tutto il resto scorre**
-                      Expanded(
-                        child: SingleChildScrollView(
-                          physics: const ClampingScrollPhysics(),
-                          child: Container(
-                            color: backgroundColor,
-                            child: Column(
-                              children: [
-                                const SizedBox(height: 16),
-                                Center(
-                                  child: getPokemonImage(
-                                    id: updatedPokemon.id!,
-                                    dimensione: 200,
-                                  ),
-                                ),
-                                PokemonDetailsBanner(pokemon: updatedPokemon),
-                              ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_back,
+                              size: 32,
+                              color: Theme.of(context).colorScheme.primary,
                             ),
+                            onPressed: () => Navigator.pop(context),
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            splashRadius: 24,
                           ),
+                          FavSelector(pokemon: updatedPokemon),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              updatedPokemon.name.capitalize(),
+                              style: const TextStyle(fontSize: 30),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              updatedPokemon.id! < 100
+                                  ? "#0${updatedPokemon.id}"
+                                  : "#${updatedPokemon.id}",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: ColorUtil().lightGrey,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
+                ),
+              ),
+
+              /// **Tutto il resto scorre**
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: const ClampingScrollPhysics(),
+                  child: Container(
+                    color: backgroundColor,
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        Center(
+                          child: getPokemonImage(
+                            id: updatedPokemon.id!,
+                            dimensione: 200,
+                          ),
+                        ),
+                        PokemonDetailsBanner(pokemon: updatedPokemon),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
